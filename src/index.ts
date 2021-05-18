34f28b21b96caf1fc5add5bbd73536966d807b38
Thomas Wang <hi@xgwang.me>
2021-05-17 17:47:54 -0700

    fix linting

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ export { default } from './beacon';
 export * from './interfaces';
 export {
   clearQueue,
-  peekQueue,
   peekBackQueue,
+  peekQueue,
   setRetryHeaderPath,
   setRetryQueueConfig,
 } from './queue';
