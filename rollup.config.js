a047988f857477eeb760455aa0faa5d6d59f295b
Thomas Wang <hi@xgwang.me>
2021-10-26 08:41:05 -0700

    Build separate bundle file instead of overwrite

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,24 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
-export default {
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
   input: 'dist/index.js',
   plugins: [sourcemaps(), commonjs(), resolve()],
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true,
-    sourcemapExcludeSources: true,
-  },
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
 };
+
+export default config;
